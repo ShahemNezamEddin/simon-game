a05abb44dd04fd14c43115f812d7b0effaa5a453
ShahemNezamEddin <shahem.nezam.eddin@gmail.com>
2023-02-22 17:05:12 +0000

    Add jest test

--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -115,6 +115,11 @@ describe("gameplay works correctly", () => {
         playerTurn();
         expect(game.score).toBe(1);
     });
+    test("should  call an alert, if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
+    });
     test("clicking during computer sequence should fail", () => {
         showTurns();
         game.lastButton = "";
